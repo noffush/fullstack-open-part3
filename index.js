f204f63ea8a42c51552cf0cb7b2ce942cdaf15b2
noffush <chen.nofar@gmail.com>
2024-09-18 17:55:46 +0300

    Exercises 3.13-3.14

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const cors = require('cors');
+const Person = require('./models/person');
 
 app.use(cors());
 app.use(express.json());
@@ -9,48 +11,26 @@ app.use(express.static('dist'));
 morgan.token('data', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'));
 
-let persons = [
-  { 
-    id: "1",
-    name: "Arto Hellas", 
-    number: "040-123456"
-  },
-  { 
-    id: "2",
-    name: "Ada Lovelace", 
-    number: "39-44-5323523"
-  },
-  { 
-    id: "3",
-    name: "Dan Abramov", 
-    number: "12-43-234345"
-  },
-  { 
-    id: "4",
-    name: "Mary Poppendieck", 
-    number: "39-23-6423122"
-  }
-];
-
 app.get('/api/persons', (req, res) => {
-  res.json(persons);
+  Person.find({}).then(persons => {
+    res.json(persons);
+  });
 });
 
 app.get('/info', (req, res) => {
   const reqTime = (new Date()).toString();
-  res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${reqTime}</p>`);
+  Person.find({}).then(persons => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${reqTime}</p>`);
+  });
 });
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = req.params.id;
-  const person = persons.find(p => p.id === id);
-  if (person) {
+  Person.findById(req.params.id).then(person => {
     res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  });
 });
 
+// TODO:
 app.delete('/api/persons/:id', (req, res) => {
   const id = req.params.id;
   persons = persons.filter(p => p.id !== id);
@@ -58,13 +38,8 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end();
 });
 
-function generateId() {
-  return String(Math.floor(Math.random() * 9999999));
-}
-
 app.post('/api/persons', (req, res) => {
-  const name = req.body.name;
-  const number = req.body.number;
+  const {name, number} = req.body;
 
   if (!name || !number) {
     return res.status(400).json({
@@ -72,23 +47,14 @@ app.post('/api/persons', (req, res) => {
     });
   }
 
-  if (persons.some(p => p.name === name)) {
-    return res.status(400).json({
-      error: 'name must be unique'
-    });
-  }
+  const newPerson = new Person({ name, number });
 
-  const person = {
-    id: generateId(),
-    name,
-    number
-  };
-  
-  persons.push(person);
-  res.json(person);
+  newPerson.save().then(person => {
+    res.json(person);
+  });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
